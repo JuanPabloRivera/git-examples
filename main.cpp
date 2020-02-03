c64d6f2d3174947ffec78886c32c7501575813a5
Juan Pablo Rivera Ruiz <jprr.2000@hotmail.com>
2020-02-03 14:38:24 -0600

    Mi primer commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int add(int a, int b){
+    int result = a + b;
+    return result;
+}
+
+int main(){
+    int suma = add(45,23);
+    cout << suma << endl;
+}
